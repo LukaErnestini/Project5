e5c807a2b8210deb9e0e6ef4ca73500dcf827f3e
d0m3nS <domen.stepancic2@gmail.com>
2024-11-25 00:18:09 +0100

    implement categories CRUD

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ItemsModule } from './items/items.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [PrismaModule, ItemsModule],
+  imports: [PrismaModule, ItemsModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
