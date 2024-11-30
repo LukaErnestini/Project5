e4dde3e2b261cea793a799b8b9ec41f76c63601b
Luka Ernestini <33009862+LukaErnestini@users.noreply.github.com>
2024-11-30 19:13:09 +0100

    add a newline after constructor

--- backend/src/categories/categories.service.ts
+++ backend/src/categories/categories.service.ts
@@ -5,6 +5,7 @@ import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class CategoriesService {
+  
   constructor(private prisma: PrismaService) {}
   create(createCategoryDto: CreateCategoryDto) {
     return this.prisma.category.create({
